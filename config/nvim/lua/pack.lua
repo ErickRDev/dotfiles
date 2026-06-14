d5f1d9eb12931ffd60aa17be2b31dc7654f239a2
Erick Rocha <erickkrocha@gmail.com>
2026-06-14 16:47:05 -0300

    revamp nvim config

---
+++ config/nvim/lua/pack.lua
@@ -0,0 +1,43 @@
+local gh = function(x) return "https://github.com/" .. x end
+local pack_install_or_update = function(kind) return kind == "install" or kind == "update" end
+
+local hooks = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+
+  if name == "telescope-fzf-native.nvim" and pack_install_or_update(kind) then
+    local obj = vim.system({ "make" }, { cwd = ev.data.path }):wait()
+    if obj.code ~= 0 then
+      vim.notify(
+        ("telescope-fzf-native.nvim build failed (exit %d):\n%s"):format(obj.code, obj.stderr or ""),
+        vim.log.levels.ERROR
+      )
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("PackChanged", { callback = hooks })
+
+vim.pack.add({
+  -- ui
+  gh("nvim-lualine/lualine.nvim"),
+  gh("nvim-tree/nvim-tree.lua"),
+  -- lsp
+  gh("neovim/nvim-lspconfig"),
+  -- syntax highlighting
+  gh("nvim-treesitter/nvim-treesitter"),
+  -- formatting
+  gh("stevearc/conform.nvim"),
+  -- navigation
+  gh("christoomey/vim-tmux-navigator"),
+  gh("nvim-lua/plenary.nvim"),
+  gh("nvim-telescope/telescope.nvim"),
+  gh("nvim-telescope/telescope-fzf-native.nvim"),
+  -- style
+  gh("nvim-tree/nvim-web-devicons"),
+  { src = gh("bluz71/vim-moonfly-colors"), name = "moonfly" },
+  -- completion
+  {
+    src = gh("saghen/blink.cmp"),
+    version = vim.version.range("^1"),
+  },
+})
---
+++ config/nvim/lua/settings.lua
@@ -0,0 +1,27 @@
+local set = vim.opt
+
+set.background = "dark"
+set.termguicolors = true
+
+set.number = true         --  Display line numbers
+set.relativenumber = true -- Display relative line numbers
+
+-- Searching
+set.incsearch = true -- Search as characters are entered
+set.hlsearch = true  -- Highlight searched terms
+
+-- Status bar
+set.laststatus = 2
+-- set.noshowmode = true
+
+-- Pane splits
+set.splitbelow = true
+set.splitright = true
+
+-- Symbols
+set.listchars = "tab:→ ,space:·,nbsp:␣,trail:•,precedes:«,extends:»"
+set.list = true
+
+set.shiftwidth = 2
+set.tabstop = 2
+set.expandtab = true
